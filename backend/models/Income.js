a71cec70d8164df5623ef6f83657d6baa9cf1aa7
sahiba0915 <sahibakumari0915@outlook.com>
2025-05-05 19:30:28 +0530

    removed unwanted space

--- backend/models/Income.js
+++ backend/models/Income.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const IncomeSchema = new mongoose.Schema({
-
     userId: {type: mongoose.Schema.Types.ObjectId, ref:"User", required: true},
     icon: {type: String},
     source: {type: String, required: true},
