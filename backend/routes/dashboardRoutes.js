2be320db9bc7d739a44261d41415c427b1502609
sahiba0915 <sahibakumari0915@outlook.com>
2025-05-05 20:26:14 +0530

    fix: env 2

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -3,7 +3,6 @@ const {protect} = require("../middleware/authMiddleware");
 const {getDashboardData} = require("../controllers/dashboardController");
 
 const router = express.Router();
-
 router.get("/", protect, getDashboardData);
 
 module.exports = router;
